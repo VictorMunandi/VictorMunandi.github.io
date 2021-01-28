f1acb401718ebfbd847d1143a470a85c12e9f8a6
unknown <victormunandi4@gmail.com>
2021-01-28 20:02:57 +0300

    second commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-(function($) {
+!(function($) {
   "use strict";
 
   // Hero typed
@@ -37,6 +37,21 @@
           $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
         }
         return false;
+      }
+    }
+  });
+
+  $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $('body').toggleClass('mobile-nav-active');
+    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  });
+
+  $(document).click(function(e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
       }
     }
   });
